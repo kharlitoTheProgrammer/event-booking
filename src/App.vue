70a98a61b05ab7d3f1763823ee64139656af896c
kharl <kharlchrisanaquino@gmail.com>
2025-03-12 19:37:54 +0800

    First commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import EventCard from "@/components/EventCard.vue";
+</script>
+
+<template>
+  <main class="container mx-auto my-8 space-y-8">
+    <h1 class="text-4xl font-medium">Event Booking App</h1>
+
+    <h2 class="text-2xl font-medium">All Events</h2>
+
+    <div class="grid grid-cols-2 gap-8">
+      <EventCard
+        v-for="i in 8"
+        :key="i"
+        title="Vue Conference"
+        when="3-11-2025"
+        description="Vue conference where we'll talk about JavaScript and Vue"
+      />
+    </div>
+
+    <h2 class="text-2xl font-medium">All Bookings</h2>
+  </main>
+</template>
---
+++ src/components/EventCard.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  title: String,
+  when: String,
+  description: String,
+});
+</script>
+
+<template>
+  <div class="border border-gray-800 bg-white rounded-md">
+    <h2 class="text-xl p-4 border-b border-gray-400 font-medium">
+      {{ title }}
+    </h2>
+    <p class="p-4 border-b border-gray-400">{{ when }}</p>
+    <p class="p-4">{{ description }}</p>
+  </div>
+</template>
