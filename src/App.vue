079eeac7cf89347d105de65fd6b94a4acea61e46
kharl <kharlchrisanaquino@gmail.com>
2025-03-15 15:09:17 +0800

    Add fetching of data from API

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,19 @@
     <h2 class="text-2xl font-medium">All Events</h2>
 
     <div class="grid grid-cols-2 gap-8">
-      <EventCard
-        v-for="i in 8"
-        :key="i"
-        title="Vue Conference"
-        when="3-11-2025"
-        description="Vue conference where we'll talk about JavaScript and Vue"
-        @register="console.log('Registered!')"
-      />
+      <template v-if="!eventsLoading">
+        <EventCard
+          v-for="event in events"
+          :key="event.id"
+          :title="event.title"
+          :when="event.date"
+          :description="event.description"
+          @register="console.log('Registered!')"
+        />
+      </template>
+      <template v-else>
+        <LoadingEventCard v-for="i in 4" :key="i"></LoadingEventCard>
+      </template>
     </div>
 
     <h2 class="text-2xl font-medium">Your Bookings</h2>
@@ -25,4 +30,26 @@
 <script setup>
 import EventCard from "@/components/EventCard.vue";
 import BookingItem from "./components/BookingItem.vue";
+import { onMounted, ref } from "vue";
+import LoadingEventCard from "./components/LoadingEventCard.vue";
+
+// variable for the events
+const events = ref([]);
+const eventsLoading = ref(false);
+
+// fetch events function (use async await)
+const fetchEvents = async () => {
+  eventsLoading.value = true;
+  try {
+    const res = await fetch("http://localhost:3001/events");
+    events.value = await res.json();
+  } finally {
+    eventsLoading.value = false;
+  }
+};
+
+// use onMounted
+onMounted(() => {
+  fetchEvents();
+});
 </script>
---
+++ src/components/LoadingEventCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <RoundedCard class="animate-pulse">
+    <div class="p-4 border-b border-gray-300 h-6 bg-gray-200"></div>
+    <div class="p-4 border-b border-gray-300 h-4 bg-gray-200"></div>
+    <div class="p-4 h-4 bg-gray-200"></div>
+    <div class="flex justify-end p-4">
+      <div class="bg-gray-200 h-8 w-24 rounded-full"></div>
+    </div>
+  </RoundedCard>
+</template>
+
+<script setup>
+import RoundedCard from "@/components/RoundedCard.vue";
+</script>
