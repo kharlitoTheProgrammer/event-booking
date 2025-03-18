d7979352198f53cc93bec406ebf8af6c2b999f4b
kharl <kharlchrisanaquino@gmail.com>
2025-03-18 23:40:37 +0800

    Add LoadingBookingItem and use it in App.vue

---
+++ src/components/LoadingBookingItem.vue
@@ -0,0 +1,12 @@
+<template>
+  <SectionCard class="animate-pulse">
+    <div class="flex space-x-4 justify-between">
+      <div class="h-8 bg-gray-300 rounded-full w-1/3"></div>
+      <div class="h-8 bg-gray-300 rounded-full w-24"></div>
+    </div>
+  </SectionCard>
+</template>
+
+<script setup>
+import SectionCard from "./SectionCard.vue";
+</script>
